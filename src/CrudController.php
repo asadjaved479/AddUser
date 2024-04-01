06b8ff9c808a84a15a556ef7e3902f0929aa74d1
asadjaved479 <77199598+asadjaved479@users.noreply.github.com>
2024-04-02 03:07:27 +0500

    kmk

---
+++ src/CrudController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AsadJaved\AddUser;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CrudController extends Controller
+{
+    //
+
+    public function show()
+    {
+        return "This Package is working fine Asad";
+    }
+}
